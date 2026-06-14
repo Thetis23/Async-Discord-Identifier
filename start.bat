947628f99893fa47e0675034e2c41c69d0210af7
Thetis23 <yusufarslan122y@gmail.com>
2026-06-14 22:36:24 +0300

    start.bat

---
+++ start.bat
@@ -0,0 +1,8 @@
+@off
+python checker.py
+echo.
+echo ======================================================
+echo Program durduruldu veya bir hata olustu.
+echo Ekranin kapanmamasi icin asagidaki tusa basin...
+echo ======================================================
+pause
